a4968b099bf0b46936d36a37ca3c4aedcf9c4bf0
ahmed hamada <ahmedhamada496@gmail.com>
2023-05-06 23:34:33 +0300

    add reset password functionality

---
+++ utils/email.js
@@ -0,0 +1,27 @@
+const nodemailer = require("nodemailer")
+const mailConfig = require("./../config/mailConfig")
+
+const sendEmail = async (options) => {
+  // Define the transporter
+  const transporter = nodemailer.createTransport({
+    host: "smtp-relay.sendinblue.com",
+    port: 587,
+    auth: {
+      user: "ahmedhamada496@gmail.com",
+      pass: mailConfig.brevoSMTPkey,
+    },
+  })
+  // 2) Define the email options
+  const mailOptions = {
+    from: "Ahmed Hamada <hello@makhlouf.io>",
+    to: options.email,
+    subject: options.subject,
+    text: options.message,
+    // html:
+  }
+
+  // 3) Actually send the email
+  await transporter.sendMail(mailOptions)
+}
+
+module.exports = sendEmail
